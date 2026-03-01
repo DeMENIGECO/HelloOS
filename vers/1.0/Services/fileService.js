da2f94b32d4767c19815922b3a8dadc0dc32d3f7
DeMENIGECO <domenigeco@gmail.com>
2026-03-01 19:48:08 +0100

    Delete vers/1.0/Services directory

--- vers/1.0/Services/fileService.js
+++
@@ -1,9 +0,0 @@
-Kernel.registerService("files", {
-
-  files:["notes.txt","todo.txt","photo.png"],
-
-  list(){
-    return this.files;
-  }
-
-});
--- vers/1.0/Services/searchService.js
+++
@@ -1,9 +0,0 @@
-Kernel.registerService("search", {
-
-  searchApps(query){
-    return Kernel.apps.filter(a =>
-      a.name.toLowerCase().includes(query.toLowerCase())
-    );
-  }
-
-});
